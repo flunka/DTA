a7c96a3aed396bd11fd3bee0bc97d353f3c9fbad
Flunka <flunka@gmail.com>
2019-03-05 13:13:55 +0100

    ADD: GlobalMethod to backend

--- app/http/api/api.py
+++ app/http/api/api.py
@@ -1,19 +1,45 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+
+import time
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class DTA(Resource):
   """docstring for HelloWorld"""
 
+  def post(self):
+    return self.get()
+
   def get(self):
-    return {'hello': 'world'}
+    self.parser = reqparse.RequestParser()
+    args = self.parser.parse_args()
+    time.sleep(5)
+    for arg in args:
+      print(arg)
+    return {'DTA': 'by Piotr Banas'}, {'Access-Control-Allow-Origin': '*'}
 
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(HelloWorld, '/hello')
+class GlobalMethod(Resource):
+  """docstring for GlobalMethod"""
+
+  def __init__(self):
+    super().__init__()
+    self.parser = reqparse.RequestParser()
+    self.parser.add_argument('test', type=bool, required=True)
+
+  def post(self):
+    args = self.parser.parse_args()
+    if args['test']:
+      return {'true': ''}
+    else:
+      return {'false': ''}
+
+
+api.add_resource(DTA, '/', '/DTA')
+api.add_resource(GlobalMethod, '/GlobalMethod')
 
 if __name__ == '__main__':
   app.run(debug=True)
