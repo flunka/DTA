075be01f676f249d3787bee87798e3937bf575c5
Flunka <flunka@gmail.com>
2019-03-11 13:32:53 +0100

    ADD: env API_URL variable

--- app/http/web/webpack.config.js
+++ app/http/web/webpack.config.js
@@ -1,7 +1,34 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const fs = require('fs');
 
 
-const config = {
+
+
+module.exports = (env) =>{
+
+
+  const currentPath = path.join(__dirname);
+    
+  // Create the fallback path (the production .env)
+  const basePath = currentPath + '/.env';
+
+  // We're concatenating the environment name to our filename to specify the correct env file!
+  const envPath = basePath + '.' + env.ENVIRONMENT;
+
+  // Check if the file exists, otherwise fall back to the production .env
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+
+    // Set the path parameter in the dotenv config
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+
+  return {
 
   watch: true,
 
@@ -37,6 +64,10 @@ const config = {
     ],
   },
 
+  plugins: [
+    new webpack.DefinePlugin(envKeys)
+  ],
+
   devServer: {
         contentBase: "./",
         inline: true,
@@ -44,4 +75,4 @@ const config = {
     },
 };
 
-module.exports = config;
+};
