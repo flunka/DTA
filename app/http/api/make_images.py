0b17ae843c592dbf5e8dd10829605b356cf4c4c0
Flunka <flunka@gmail.com>
2019-03-15 08:49:11 +0100

    FIX: Loading images

--- app/http/api/make_images.py
+++ app/http/api/make_images.py
@@ -15,17 +15,18 @@ def make_applied_dose_image(file_name, path):
       elif(dose and "COL" in line):
         col = True
       elif(dose and not col):
-        data.append([float(x) * 2.5 for x in line.split("\t")[2:-1]])
+        data.append([float(x) for x in line.split("\t")[2:-1]])
       elif col:
         dataX = [float(x) for x in line.split('\t')[2:-1]]
         col = False
         dose = False
         break
-  colunms, rows = np.shape(np.array(data[1:]))
+  doses = np.array(data[1:])
+  colunms, rows = np.shape(doses)
   image = np.zeros([colunms, rows, 3])
-  image[:, :, 0] = np.array(data[1:])
-  image[:, :, 1] = np.array(data[1:])
-  image[:, :, 2] = np.array(data[1:])
+  image[:, :, 0] = doses
+  image[:, :, 1] = doses
+  image[:, :, 2] = doses
   image = cv.resize(image, None, fx=5, fy=5, interpolation=cv.INTER_CUBIC)
   cv.imwrite("".join((path, "/applied.jpg")), image)
 
@@ -38,10 +39,11 @@ def make_planned_dose_image(file_name, path):
       if(idx == 5):
         dataX = [float(x) / 10 for x in line.split("\t")[1:-1]]
       elif(idx > 5):
-        data.append([float(x) * 2.5 for x in line.split("\t")[1:-1]])  # Zapytać
-  colunms, rows = np.shape(np.array(data))
+        data.append([float(x) for x in line.split("\t")[1:-1]])  # Zapytać
+  doses = np.array(data)
+  colunms, rows = np.shape(doses)
   image = np.zeros([colunms, rows, 3])
-  image[:, :, 0] = np.array(data)
-  image[:, :, 1] = np.array(data)
-  image[:, :, 2] = np.array(data)
+  image[:, :, 0] = doses
+  image[:, :, 1] = doses
+  image[:, :, 2] = doses
   cv.imwrite("".join((path, "plan.jpg")), image)
