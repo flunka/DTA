7384525f838473affd0c574b3ed2296ed46090f6
Flunka <flunka@gmail.com>
2019-03-27 12:40:34 +0100

    MODIFY: Change default values for parameters

--- app/http/web/src/CommonOptions.jsx
+++ app/http/web/src/CommonOptions.jsx
@@ -40,40 +40,34 @@ function CommonOptions(props) {
           <Form.Group as={Row} controlId="formThresholdGammaIndex">
             <Form.Label column sm="9">Threshold for Gamma index method</Form.Label>
             <Col sm="3">
-              <Form.Control type="number" placeholder="0.00" min="0" max="99.99" step="0.01" />
+              <Form.Control type="number" defaultValue={1.00} 
+                defaultValue="1.00" min="0" max="99.99" step="0.01" />
             </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="formMinimalValuesPercentage">
             <Form.Label column sm="9">Do not accout for percentage of minimal values</Form.Label>
             <Col sm="3">
-              <Form.Control type="number" placeholder="0" min="0" max="30"/>
+              <Form.Control type="number" defaultValue={0.1} 
+                defaultValue="0" min="0" step="0.1" max="30"/>
             </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="formMaximalDistanceToBeProbed">
             <Form.Label column sm="9">Maximal distance to be probed for DTA [mm]</Form.Label>
             <Col sm="3">
-              <Form.Control type="number" placeholder="0.00" min="0" max="99.99" step="0.01"/>
+              <Form.Control type="number" defaultValue="0.00" min="0" max="99.99" step="0.01"/>
             </Col>
           </Form.Group> 
         </Col>
         <Col>{/* Linear Coefficients*/}
           <Row noGutters className='ml-1'>
-            <label>Linear model for standard deviation of delivered dose: SD(Dose)=A+B*Dose</label>
+            <label>Linear model for standard deviation of delivered dose: SD(Dose)=A*Dose</label>
           </Row>
           <Row noGutters>
             <Col>
               <Form.Group className='ml-1' as={Row} noGutters controlId="formLinearCoefficientA">
                 <Form.Label column sm="2">A</Form.Label>
                 <Col sm="10">
-                  <Form.Control type="number" placeholder="0.00" min="0" max="99.99" step="0.01" />
-                </Col>
-              </Form.Group>
-            </Col>
-            <Col>
-                <Form.Group className='ml-1' as={Row} noGutters controlId="formLinearCoefficientB">
-                <Form.Label column sm="2">B</Form.Label>
-                <Col sm="10">
-                  <Form.Control inline="true" type="number" placeholder="0.00" min="0" max="99.99" step="0.01" />
+                  <Form.Control type="number" defaultValue="0.1" min="0" max="99.99" step="0.01" />
                 </Col>
               </Form.Group>
             </Col>
--- app/http/web/src/Header.jsx
+++ app/http/web/src/Header.jsx
@@ -13,7 +13,7 @@ function Header(argument) {
         height="30"
         className="d-inline-block align-top"
       />
-      {' DTA App | v0.6'}
+      {' DTA App | v0.6.1'}
     </Navbar.Brand>
   </Navbar>
     )
